64bca82a2deaef7c4429d2fcb43567259fc224b1
Vikas Kaushik <kaushikvk2607@gmail.com>
2025-02-28 18:51:50 +0000

    correcting port

--- tourney-nation-backend/index.js
+++ tourney-nation-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+
 var cors = require('cors');
 app.use(cors())
 app.use(express.json())
